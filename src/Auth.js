bb7f720dba3460ed07fa85cc66e8e89442198021
Данил <abanin-02@mail.ru>
2019-11-17 21:51:12 +0400

    Create form authorization

--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import {Form, Button, Navbar, Alert} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Components/auth.css'
 class Auth extends React.Component {
     state = {
         email: '',
@@ -20,15 +22,49 @@ class Auth extends React.Component {
     render() {
         return (
             <div>
-                <form method="POST" action="#">
-                    <label for="email">Email: </label>
-                    <input name="email" id="email" onChange={this.handleChangeEmail} />
-                    <br></br>
-                    <label for="password">Password: </label>
-                    <input name="password" id="password" type="password" onChange={this.handleChangePassword}/>
-                    <br></br>
-                    <button>Login</button>
-                </form>
+                <div>
+                    <Navbar bg="dark" expand="lg" variant="dark">
+                        <Navbar.Brand href="#home">PixelNetwork</Navbar.Brand>
+                    </Navbar>
+                </div>
+                <div className={'auth'}>
+                    <form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Логин</Form.Label>
+                            <Form.Control type="email" placeholder="Логин" onChange={this.handleChangeEmail}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Пароль</Form.Label>
+                            <Form.Control type="password" placeholder="Пароль" onChange={this.handleChangePassword}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicChecbox">
+                            <Form.Check type="checkbox" label="Запомни меня"/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Войти
+                        </Button>
+                    </form>
+                </div>
+                <div className={'sign'}>
+                    <form>
+                        <h3>Впервые в PixelNetwork</h3>
+                        <hr/>
+                        <div className={'sign_box'}>
+                            <Form.Control  placeholder="Имя" onChange={this.handleChangeEmail}/>
+                            <Form.Control  className={'name'} placeholder="Фамилия" onChange={this.handleChangePassword}/>
+                        </div>
+                        <br/>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control placeholder="Номер телефона или почта" onChange={this.handleChangeEmail}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control placeholder="Пароль" onChange={this.handleChangePassword}/>
+                        </Form.Group>
+                        <Button className={'button_auth'} variant="success" type="submit">
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Auth from './Auth'
 import * as serviceWorker from './serviceWorker';
+import {Route, Switch, Router, Redirect} from "react-router";
+import history from './history'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    <Router history={history}>
+        <Switch>
+            <Route path={"/login/"} component={Auth}/>
+        </Switch>
+    </Router>,
+    document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
