6be374e6dbc2ddf4ae00897ef655287de22404c5
Данил <abanin-02@mail.ru>
2019-11-18 22:08:57 +0400

    authorization design fixed

--- src/Auth.js
+++ src/Auth.js
@@ -37,10 +37,10 @@ class Auth extends React.Component {
                     <div className={'auth'}>
                         <form>
                             <Form.Group controlId="formBasicEmail">
-                                <Form.Control type="email" placeholder="Логин" onChange={this.handleChangeEmail}/>
+                                <Form.Control type="email" placeholder="Логин" onChange={this.handleChangeEmail} style={{backgroundColor: '#eddefc'}}/>
                             </Form.Group>
                             <Form.Group controlId="formBasicPassword">
-                                <Form.Control type="password" placeholder="Пароль" onChange={this.handleChangePassword}/>
+                                <Form.Control type="password" placeholder="Пароль" onChange={this.handleChangePassword} style={{backgroundColor: '#eddefc'}}/>
                             </Form.Group>
                             <Form.Group controlId="formBasicChecbox">
                                 <Form.Check type="checkbox" label="Запомни меня"/>
@@ -56,15 +56,15 @@ class Auth extends React.Component {
                             <hr/>
                             <Form.Group controlId="formBasicEmail">
                                 <div className={'sign_box'}>
-                                    <Form.Control  placeholder="Имя" onChange={this.handleChangeEmail}/>
-                                    <Form.Control  className={'name'} placeholder="Фамилия" onChange={this.handleChangePassword}/>
+                                    <Form.Control  placeholder="Имя" onChange={this.handleChangeEmail} style={{backgroundColor: '#eddefc'}}/>
+                                    <Form.Control  className={'name'} placeholder="Фамилия" onChange={this.handleChangePassword} style={{backgroundColor: '#eddefc'}}/>
                                 </div>
                             </Form.Group>
                             <Form.Group controlId="formBasicEmail">
-                                <Form.Control placeholder="Номер телефона или почта" onChange={this.handleChangeEmail}/>
+                                <Form.Control placeholder="Номер телефона или почта" onChange={this.handleChangeEmail} style={{backgroundColor: '#eddefc'}}/>
                             </Form.Group>
                             <Form.Group controlId="formBasicPassword">
-                                <Form.Control placeholder="Пароль" onChange={this.handleChangePassword}/>
+                                <Form.Control placeholder="Пароль" onChange={this.handleChangePassword} style={{backgroundColor: '#eddefc'}}/>
                             </Form.Group>
                             <Button className={'button_auth'} variant="success" type="submit">
                                 Зарегистрироваться
