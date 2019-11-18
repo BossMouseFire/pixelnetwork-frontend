fdd490ef1a292ede83862d4360bc68084e1bd308
Данил <abanin-02@mail.ru>
2019-11-18 21:59:50 +0400

    Design

--- src/Auth.js
+++ src/Auth.js
@@ -23,47 +23,55 @@ class Auth extends React.Component {
         return (
             <div>
                 <div>
-                    <Navbar bg="dark" expand="lg" variant="dark">
-                        <Navbar.Brand href="#home">PixelNetwork</Navbar.Brand>
+                    <Navbar  expand="lg" variant="dark" className={'logo'}>
+                        <Navbar.Brand href="#home" >
+                            <a>PixelNetwork</a>
+                        </Navbar.Brand>
                     </Navbar>
                 </div>
-                <div className={'auth'}>
-                    <form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Логин</Form.Label>
-                            <Form.Control type="email" placeholder="Логин" onChange={this.handleChangeEmail}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Пароль</Form.Label>
-                            <Form.Control type="password" placeholder="Пароль" onChange={this.handleChangePassword}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicChecbox">
-                            <Form.Check type="checkbox" label="Запомни меня"/>
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Войти
-                        </Button>
-                    </form>
-                </div>
-                <div className={'sign'}>
-                    <form>
-                        <h3>Впервые в PixelNetwork</h3>
-                        <hr/>
-                        <div className={'sign_box'}>
-                            <Form.Control  placeholder="Имя" onChange={this.handleChangeEmail}/>
-                            <Form.Control  className={'name'} placeholder="Фамилия" onChange={this.handleChangePassword}/>
-                        </div>
-                        <br/>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Control placeholder="Номер телефона или почта" onChange={this.handleChangeEmail}/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Control placeholder="Пароль" onChange={this.handleChangePassword}/>
-                        </Form.Group>
-                        <Button className={'button_auth'} variant="success" type="submit">
-                            Зарегистрироваться
-                        </Button>
-                    </form>
+                <div className={'box'}>
+                    <div>
+                        <img src={require("./image/logo.png")} className={'packman'} alt={"Лого"}/>
+                    </div>
+                    <div>
+                    <div className={'auth'}>
+                        <form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="Логин" onChange={this.handleChangeEmail}/>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Пароль" onChange={this.handleChangePassword}/>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicChecbox">
+                                <Form.Check type="checkbox" label="Запомни меня"/>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Войти
+                            </Button>
+                        </form>
+                    </div>
+                    <div className={'sign'}>
+                        <form>
+                            <h3>Впервые в PixelNetwork</h3>
+                            <hr/>
+                            <Form.Group controlId="formBasicEmail">
+                                <div className={'sign_box'}>
+                                    <Form.Control  placeholder="Имя" onChange={this.handleChangeEmail}/>
+                                    <Form.Control  className={'name'} placeholder="Фамилия" onChange={this.handleChangePassword}/>
+                                </div>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Control placeholder="Номер телефона или почта" onChange={this.handleChangeEmail}/>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Control placeholder="Пароль" onChange={this.handleChangePassword}/>
+                            </Form.Group>
+                            <Button className={'button_auth'} variant="success" type="submit">
+                                Зарегистрироваться
+                            </Button>
+                        </form>
+                    </div>
+                    </div>
                 </div>
             </div>
         )
